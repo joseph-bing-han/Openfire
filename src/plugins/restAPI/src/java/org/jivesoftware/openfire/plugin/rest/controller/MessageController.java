71d405e454fbfbf7ec0bb60b89c47a487b067aed
Joseph Han <hanbingbing1@gmail.com>
2018-03-07 14:35:28 +1300

    Modify for Xebni
    Broadcast message could send to offline users

--- src/plugins/restAPI/src/java/org/jivesoftware/openfire/plugin/rest/controller/MessageController.java
+++ src/plugins/restAPI/src/java/org/jivesoftware/openfire/plugin/rest/controller/MessageController.java
@@ -6,12 +6,20 @@ import org.jivesoftware.openfire.SessionManager;
 import org.jivesoftware.openfire.plugin.rest.entity.MessageEntity;
 import org.jivesoftware.openfire.plugin.rest.exceptions.ExceptionType;
 import org.jivesoftware.openfire.plugin.rest.exceptions.ServiceException;
+import org.xmpp.packet.Message;
+import org.jivesoftware.openfire.XMPPServer;
+import org.jivesoftware.openfire.user.User;
+import org.jivesoftware.openfire.user.UserManager;
+import org.jivesoftware.openfire.PresenceManager;
+import org.jivesoftware.util.JiveGlobals;
 
 /**
  * The Class MessageController.
  */
 public class MessageController {
-    /** The Constant INSTANCE. */
+    /**
+     * The Constant INSTANCE.
+     */
     public static final MessageController INSTANCE = new MessageController();
 
     /**
@@ -26,18 +34,37 @@ public class MessageController {
     /**
      * Send broadcast message.
      *
-     * @param messageEntity
-     *            the message entity
-     * @throws ServiceException
-     *             the service exception
+     * @param messageEntity the message entity
+     * @throws ServiceException the service exception
      */
     public void sendBroadcastMessage(MessageEntity messageEntity) throws ServiceException {
         if (messageEntity.getBody() != null && !messageEntity.getBody().isEmpty()) {
-            SessionManager.getInstance().sendServerMessage(null, messageEntity.getBody());
+            try {
+                Message message = new Message();
+                message.setType(Message.Type.chat);
+                message.setBody(messageEntity.getBody());
+                String xmppdomain = "@" + JiveGlobals.getProperty("xmpp.domain");
+                message.setFrom("system" + xmppdomain);//目前不加from则会导致客户端不能自动获取离线消息，除主动获取。
+
+                XMPPServer server = XMPPServer.getInstance();
+                UserManager userManager = server.getUserManager();
+                PresenceManager presenceManager = server.getPresenceManager();
+
+                for (User user : UserManager.getInstance().getUsers()) {
+                    String username = user.getUsername();
+                    message.setTo(username + xmppdomain);
+                    if (presenceManager.isAvailable(user)) {
+                        server.getRoutingTable().broadcastPacket(message, false);
+                    } else {
+                        server.getOfflineMessageStrategy().storeOffline(message);
+                    }
+                }
+            } catch (Exception e) {
+            }
         } else {
             throw new ServiceException("Message content/body is null or empty", "",
-                    ExceptionType.ILLEGAL_ARGUMENT_EXCEPTION,
-                    Response.Status.BAD_REQUEST);
+                ExceptionType.ILLEGAL_ARGUMENT_EXCEPTION,
+                Response.Status.BAD_REQUEST);
         }
     }
 
